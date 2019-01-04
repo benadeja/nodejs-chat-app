53961ab9b1c33596582ae482523612b095083239
jbenade <jaco.benade@momentum.co.za>
2019-01-04 14:07:26 +0200

    Add Socket io events

--- server/server.js
+++ server/server.js
@@ -1,18 +1,35 @@
 const path = require('path');
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
-/**
- * Setup Express app and tell middleware to serve static content
- */
+// Setting up an http server and telling Express to use that server
 var app = express();
+var server = http.createServer(app);
+
+// Create a Web Socket server to listen to and emit events
+var io = socketIO(server);
+
+// Tell Express to server up static content
 app.use(express.static(publicPath));
 
-/**
- * Bind Express to listener port
- */
-app.listen(port, () => {
+// Listen for a new connection and handle connection and disconnection event handlers
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.on('connect', () => {
+    console.log('User connected to server');
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User was disconnected from server');
+  });
+});
+
+
+server.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
